190329fd6f0010bde119b3a8ac3a27b188032bf6
VisiGoyo <79362138+gvisiedo@users.noreply.github.com>
2026-06-05 07:28:08 +0200

    working on ui.js

---
+++ frontend/productos/js/ui.js
@@ -0,0 +1,8 @@
+// Mostrar lista de productos en el div#productos
+export function mostrarProductos(productos) {  }
+
+// Mostrar u ocultar el formulario según el rol
+export function mostrarFormulario(esAdmin) { }
+
+// Mostrar mensaje de error
+export function mostrarError(mensaje) {}
