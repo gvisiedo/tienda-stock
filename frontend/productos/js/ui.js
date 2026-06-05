fe6f940e9d260f69ded549df1ec7550907a881e7
VisiGoyo <79362138+gvisiedo@users.noreply.github.com>
2026-06-05 08:01:48 +0200

    working on ui.js, mostrarProductos

--- frontend/productos/js/ui.js
+++ frontend/productos/js/ui.js
@@ -1,5 +1,19 @@
+const listaProductos = document.querySelector('#productos');
+
 // Mostrar lista de productos en el div#productos
-export function mostrarProductos(productos) {  }
+export function mostrarProductos(productos) {
+    productos.forEach(function(producto){
+        const div = document.createElement('div')
+        div.innerHTML=`
+        <p>Nombre: ${producto.nombre}</p>
+        <p>Precio: ${producto.precio}</p>
+        <p>Cantidad: ${producto.cantidad}</p>
+        <p>En Stock: ${producto.enStock ?'Si': 'No'}</p>
+
+        `
+        listaProductos.appendChild(div)
+    })
+  }
 
 // Mostrar u ocultar el formulario según el rol
 export function mostrarFormulario(esAdmin) { }
